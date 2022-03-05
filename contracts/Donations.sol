521765f40a825e6a4f6c1ef0b96f8e72a4360f4d
rdayanov <dayanov.radmir@gmail.com>
2022-03-05 22:05:15 +0300

    feat: Donations.sol

---
+++ contracts/Donations.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.8.0;
+
+// todo: replace with import '@openzeppelin/contracts/utils/structs/EnumerableMap.sol' after 4.6.0 released
+import './EnumerableMap.sol';
+
+contract Donations {
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
+
+    EnumerableMap.AddressToUintMap private contributors;
+
+    address private owner;
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            'Withdraw operation available only for Contract`s owner'
+        );
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function Donate() public payable {
+        address contributor = msg.sender;
+
+        if (contributors.contains(contributor)) {
+            uint256 contributedSum = contributors.get(contributor);
+            contributors.set(contributor, contributedSum + msg.value);
+        } else {
+            contributors.set(contributor, msg.value);
+        }
+    }
+
+    function Withdraw(address payable target, uint256 amount) public onlyOwner {
+        require(address(this).balance >= amount, 'Not enough funds');
+        target.transfer(amount);
+    }
+
+    function GetContributorSum(address contributor)
+        public
+        view
+        returns (uint256)
+    {
+        return contributors.get(contributor);
+    }
+
+    function GetDonationsSum() public view returns (uint256) {
+        uint256 sum = 0;
+        for (uint256 i = 0; i < contributors.length(); i++) {
+            (address _contributorAddress, uint256 contribution) = contributors
+                .at(i);
+            sum += contribution;
+        }
+        return sum;
+    }
+
+    function GetContributorsList() public view returns (address[] memory) {
+        address[] memory contributorsList = new address[](
+            contributors.length()
+        );
+        for (uint256 i = 0; i < contributors.length(); i++) {
+            (address contributor, uint256 _contributionsSum) = contributors.at(
+                i
+            );
+            contributorsList[i] = contributor;
+        }
+        return contributorsList;
+    }
+}
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,162 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import { Signer } from 'ethers';
+import type { Donations } from '../typechain';
 
-describe('Greeter', function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory('Greeter');
-    const greeter = await Greeter.deploy('Hello, world!');
-    await greeter.deployed();
+type SignerWithAddress = {
+  signer: Signer;
+  address: string;
+};
 
-    expect(await greeter.greet()).to.equal('Hello, world!');
+describe('Donations', () => {
+  let donate: Donations;
+  let contractOwner: SignerWithAddress,
+    firstContributor: SignerWithAddress,
+    anotherContributor: SignerWithAddress;
 
-    const setGreetingTx = await greeter.setGreeting('Hola, mundo!');
+  before(async () => {
+    const [owner, firstSigner, anotherSigner] = await ethers.getSigners();
+    firstContributor = { signer: firstSigner, address: firstSigner.address };
+    anotherContributor = {
+      signer: anotherSigner,
+      address: anotherSigner.address,
+    };
+    contractOwner = {
+      signer: owner,
+      address: owner.address,
+    };
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  beforeEach(async () => {
+    const Donations = await ethers.getContractFactory('Donations');
+    donate = await Donations.deploy();
+    await donate.deployed();
+  });
 
-    expect(await greeter.greet()).to.equal('Hola, mundo!');
+  describe('Donate', () => {
+    describe('firstContributor donates 123 native coins', () => {
+      const firstDonationSum = 123;
+
+      beforeEach(async () => {
+        const firstContributionTx = await donate
+          .connect(firstContributor.signer)
+          .Donate({ value: firstDonationSum });
+        await firstContributionTx.wait();
+      });
+
+      it('contributions sum from firstContributor should be 123', async () => {
+        expect(
+          await donate.GetContributorSum(firstContributor.address)
+        ).to.equal(firstDonationSum);
+      });
+
+      it('all donations sum should be 123', async () => {
+        expect(await donate.GetDonationsSum()).to.equal(firstDonationSum);
+      });
+
+      it('contributor`s address should be added to contributors list', async () => {
+        expect(await donate.GetContributorsList()).to.contain(
+          firstContributor.address
+        );
+      });
+
+      describe('firstContributor donates 44 native coins', () => {
+        const secondDonationSum = 44;
+        const donationsSum = firstDonationSum + secondDonationSum;
+        beforeEach(async () => {
+          const secondContributionTx = await donate
+            .connect(firstContributor.signer)
+            .Donate({ value: secondDonationSum });
+          await secondContributionTx.wait();
+        });
+
+        it('contributions sum should be 167', async () => {
+          expect(
+            await donate.GetContributorSum(firstContributor.address)
+          ).to.equal(donationsSum);
+        });
+
+        it('all donations sum should be 167', async () => {
+          expect(await donate.GetDonationsSum()).to.equal(donationsSum);
+        });
+
+        it('contributor`s address should be added to contributors list only once', async () => {
+          expect(await donate.GetContributorsList()).to.deep.equal([
+            firstContributor.address,
+          ]);
+        });
+      });
+
+      describe('anotherContributor donates 12 native coins', () => {
+        const anotherDonationSum = 12;
+        const donationsSum = firstDonationSum + anotherDonationSum;
+
+        beforeEach(async () => {
+          const anotherContributionTx = await donate
+            .connect(anotherContributor.signer)
+            .Donate({ value: anotherDonationSum });
+          await anotherContributionTx.wait();
+        });
+
+        it('contributions sum from anotherContributor should be 12', async () => {
+          expect(
+            await donate.GetContributorSum(anotherContributor.address)
+          ).to.equal(anotherDonationSum);
+        });
+
+        it('all donations sum should be 135', async () => {
+          expect(await donate.GetDonationsSum()).to.equal(donationsSum);
+        });
+
+        it('both contributors should be listed', async () => {
+          expect(await donate.GetContributorsList()).to.deep.equal([
+            firstContributor.address,
+            anotherContributor.address,
+          ]);
+        });
+      });
+    });
+  });
+
+  describe('Withdraw', () => {
+    const balance = 10;
+
+    beforeEach(async () => {
+      const contributionTx = await donate
+        .connect(firstContributor.signer)
+        .Donate({ value: balance });
+      await contributionTx.wait();
+    });
+
+    describe('contractOwner trying to withdraw funds', () => {
+      it('transaction should fail because of amount exceed', async () => {
+        await expect(
+          donate
+            .connect(contractOwner.signer)
+            .Withdraw(contractOwner.address, 15)
+        ).to.revertedWith('Not enough funds');
+      });
+
+      it('5 native coins should be sent to anotherContributor', async () => {
+        await expect(
+          async () =>
+            await donate
+              .connect(contractOwner.signer)
+              .Withdraw(anotherContributor.address, 5)
+        ).to.changeEtherBalance(anotherContributor.signer, 5);
+      });
+    });
+
+    describe('anotherContributor trying to withdraw funds', () => {
+      it('transaction should fail because of insufficient permissions', async () => {
+        await expect(
+          donate
+            .connect(anotherContributor.signer)
+            .Withdraw(anotherContributor.address, 5)
+        ).revertedWith(
+          'Withdraw operation available only for Contract`s owner'
+        );
+      });
+    });
   });
 });
